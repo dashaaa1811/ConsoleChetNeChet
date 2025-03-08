934aeb92f398f3cb56bfaed44cd0828d5cdfdaa8
Dasha <darabrueva65@gmail.com>
2025-03-08 11:43:30 +0300

    Добавьте файлы проекта.

---
+++ ConsoleChetNeChet/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleChetNeChet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5, };
+            foreach (var arr in array)
+            {
+                switch (arr % 2)
+                {
+                    case 0:
+                        Console.WriteLine($"{arr}-четное");
+                        break;
+                    case 1:
+                        Console.WriteLine($"{arr}-нечетное");
+                        break;
+                }
+            }
+        }
+
+    }
+}
